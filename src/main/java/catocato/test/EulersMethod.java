46588afa758dd6b9b43f0b51912ea4c542877264
catocatocato <35408004+catocatocat0@users.noreply.github.com>
2020-11-03 23:27:56 -0800

    Added JFreeChart

--- src/main/java/catocato/test/EulersMethod.java
+++ src/main/java/catocato/test/EulersMethod.java
@@ -1,5 +1,18 @@
 package catocato.test;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,21 +21,25 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EulersMethod {
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
     private File file;
 
     public static void main(String[] args){
         EulersMethod euler = new EulersMethod();
-        euler.eulersMethod();
+        euler.onEnable();
+    }
+
+    public void onEnable(){
+        eulersMethod();
     }
 
     public double equation(double x, double y){
         //what equation you want to run
-        return 0.01*y*(1-y/100);
+        return 0.01*y*(1 - y/100);
     }
 
     public void eulersMethod(){
-        double iterations;
+        int iterations;
         double stepSize;
         double y;
         double x;
@@ -31,7 +48,7 @@ public class EulersMethod {
         System.out.println("Enter the number of iterations");
         while(true) {
             try {
-                iterations = scanner.nextDouble();
+                iterations = scanner.nextInt();
                 break;
             } catch (InputMismatchException e) {
                 scanner.next();
@@ -77,25 +94,35 @@ public class EulersMethod {
 
 
         String result = "";
-
+        XYSeries series = new XYSeries("Euler's Method Graph");
         for(int i = 0; i <iterations; i++) {
-            result = result + "("+x+","+y+")\n";
-            y = y + stepSize*(equation(x,y));
+            series.add(x,y);
+            result = result + x+" "+y+"\n";
             x = x + stepSize;
+            y = y + stepSize*equation(x,y);
         }
+
+        XYSeriesCollection plotData = new XYSeriesCollection();
+
+        plotData.addSeries(series);
+
+        plot(plotData);
 
         if(outputResults(result)){
             System.out.println("Results successfully written to results.txt!");
         }
+
+
     }
 
-    public void createFile(String results){
+    public void createFile(){
         try{
             try {
                 String path = EulersMethod.class.getProtectionDomain().getCodeSource().getLocation().getPath();
                 path = URLDecoder.decode(path,"UTF-8");
                 file = new File(path);
                 file = new File(file.getParentFile().getPath(), "results.txt");
+
             }catch (NullPointerException e){
                 e.printStackTrace();
                 System.out.println("Was not able to access the file location!");
@@ -111,10 +138,55 @@ public class EulersMethod {
         }
     }
 
+    public void plot(XYDataset plotData){
+        JFreeChart chart = ChartFactory.createScatterPlot(
+                "Euler's method graph",
+                "x",
+                "y",
+                plotData,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle("Euler's Method Graph",
+                        new Font("Serif", java.awt.Font.BOLD, 18)
+                )
+        );
+
+        try {
+            ChartUtils.saveChartAsPNG(new File("plot.png"), chart, 1500, 1000);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("There was an error saving the chart image.");
+        }
+    }
+
     public boolean outputResults(String results){
         boolean enable = false;
 
-        createFile(results);
+        createFile();
 
         if(file.exists()){
             try {
